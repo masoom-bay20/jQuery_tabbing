3835a06ee1b9570e81b14d424047be3332e71fa9
MD Masoom Alam <89391692+masoom-bay20@users.noreply.github.com>
2022-01-18 16:50:47 +0545

    Add files via upload

---
+++ js/myjQuery.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $("ul li").click(function () {
+
+        $(this).addClass("selected").siblings().removeClass("selected");
+
+        $(".content > p").hide();
+
+        $($(this).data("value")).fadeIn();
+    })
+});
